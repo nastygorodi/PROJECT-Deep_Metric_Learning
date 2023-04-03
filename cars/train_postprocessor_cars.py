aab875d64bdd1f4fe8235c5c9298c1f939b7605d
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-03 12:42:14 +0300

    add postpr train files

---
+++ cars/train_postprocessor_cars.py
@@ -0,0 +1,21 @@
+import hydra
+from omegaconf import DictConfig
+
+from utils.train_postprocessor import pl_train_postprocessor
+
+from oml.registry.postprocessors import POSTPROCESSORS_REGISTRY
+from oml.registry.models import PAIRWISE_MODELS_REGISTRY
+from postprocessing.multiple_emb import MultiEmbeddingsPostprocessor
+from models.multi_query_siamese import MultiDistanceSiamese
+
+
+POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
+PAIRWISE_MODELS_REGISTRY['multi_query_siamese'] = MultiDistanceSiamese
+
+@hydra.main(config_path="configs", config_name="train_postprocessor_cars_multiple_emb.yaml")
+def main_hydra(cfg: DictConfig) -> None:
+    pl_train_postprocessor(cfg)
+
+
+if __name__ == "__main__":
+    main_hydra()
