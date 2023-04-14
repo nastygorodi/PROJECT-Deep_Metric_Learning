be13d598a7f2d321e0d2aa36dc70e2c2a9030847
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-14 13:07:09 +0300

    upd model architecture

--- models/multi_query_siamese.py
+++ models/multi_query_siamese.py
@@ -4,40 +4,72 @@ from typing import Optional, Union
 import torch
 from torch import Tensor
 
-from oml.interfaces.models import IPairwiseModel
+from models.model import IMultiQueryModel
 from oml.utils.misc_torch import elementwise_dist
-from oml.models.utils import remove_prefix_from_state_dict
-from oml.utils.io import download_checkpoint
 
+def remain_prefix_in_state_dict(state_dict, prefix: str):
+    for k in list(state_dict.keys()):
+        if k.startswith(prefix):
+            state_dict[k[len(prefix) :]] = state_dict[k]
+            del state_dict[k]
+        else:
+            del state_dict[k]
 
-class MultiDistanceSiamese(IPairwiseModel):
+    return state_dict
 
+class MultiDistanceSiamese(IMultiQueryModel):
+    
     def __init__(self, extractor, hidden_dim: int, weights: Optional[Union[str, Path]] = None, strict_load: bool = True):
         super(MultiDistanceSiamese, self).__init__()
-        #self.extractor = extractor
         
-        self.feat_dim = 224
+        self.extractor = extractor
+        
+        self.feat_dim = 384
 
-        self.proj1 = torch.nn.Linear(in_features=self.feat_dim * 3, out_features=hidden_dim, bias=False)
-        self.proj2 = torch.nn.Linear(in_features=self.feat_dim, out_features=hidden_dim, bias=False)
+        self.proj = torch.nn.Sequential(
+            torch.nn.Linear(in_features=self.feat_dim, out_features=hidden_dim // 2, bias=False),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=hidden_dim // 2, out_features=hidden_dim // 2, bias=False),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=hidden_dim // 2, out_features=hidden_dim, bias=False)
+            
+        )
         
         if weights:
-            if weights in self.pretrained_models:
-                url_or_fid, hash_md5, fname = self.pretrained_models[weights]  # type: ignore
-                weights = download_checkpoint(url_or_fid=url_or_fid, hash_md5=hash_md5, fname=fname)
 
             loaded = torch.load(weights, map_location="cpu")
             loaded = loaded.get("state_dict", loaded)
-            loaded = remove_prefix_from_state_dict(loaded, trial_key="extractor.")
+            loaded = remain_prefix_in_state_dict(loaded, prefix="model_pairwise.")
+            
             self.load_state_dict(loaded, strict=strict_load)
+        
 
-    def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
-        x1 = self.proj1(x1)
-        x2 = self.proj2(x2)
-        y = elementwise_dist(x1, x2, p=2)
+    def forward(self, x1: Tensor, x2: Tensor, train=True) -> Tensor:
+        if train:
+            x2_shape = x2.shape[-1]
+            with torch.no_grad():
+                x2_emb = self.extractor(x2)
+                x1_emb = [self.extractor(x1[:, i]) for i in range(0, x1.shape[-1] // x2_shape)]
+            
+            x2 = self.proj(x2_emb)
+            x1_new = [self.proj(item) for item in x1_emb]
+            
+            x1_ = torch.zeros_like(x2)
+            for item in x1_new:
+                x1_ += item
+            x1_ /= (x1.shape[-1] / x2_shape)
+        else:
+            x2_shape = x2.shape[-1]
+            x2 = self.proj(x2)
+            x1_new = [self.proj(x1[:, i : i + x2_shape]) for i in range(0, x1.shape[-1], x2_shape)]
+            x1_ = torch.zeros_like(x2)
+            for item in x1_new:
+                x1_ += item
+            x1_ /= (x1.shape[-1] / x2_shape)
+        y = elementwise_dist(x1_, x2, p=2)
         return y
 
-    def predict(self, x1: Tensor, x2: Tensor) -> Tensor:
-        return self.forward(x1=x1, x2=x2)
+    def predict(self, x1: Tensor, x2: Tensor, train=True) -> Tensor:
+        return self.forward(x1=x1, x2=x2, train=train)
 
 __all__ = ["MultiDistanceSiamese"]
