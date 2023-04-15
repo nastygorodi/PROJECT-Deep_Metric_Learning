fa8a11656869fcd3e53cfe34760b67d041bb1d80
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-15 20:57:56 +0300

    exclude siamese model from registry

--- cars/train_postprocessor_cars.py
+++ cars/train_postprocessor_cars.py
@@ -6,11 +6,11 @@ from utils.train_postprocessor import pl_train_postprocessor
 from oml.registry.postprocessors import POSTPROCESSORS_REGISTRY
 from oml.registry.models import PAIRWISE_MODELS_REGISTRY
 from postprocessing.multiple_emb import MultiEmbeddingsPostprocessor
-from models.multi_query_siamese import MultiDistanceSiamese
+from models.multi_query_concat import MultiConcat
 
 
 POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
-PAIRWISE_MODELS_REGISTRY['multi_query_siamese'] = MultiDistanceSiamese
+PAIRWISE_MODELS_REGISTRY['multi_query_cat'] = MultiConcat
 
 @hydra.main(config_path="configs", config_name="train_postprocessor_cars_multiple_emb.yaml")
 def main_hydra(cfg: DictConfig) -> None:
