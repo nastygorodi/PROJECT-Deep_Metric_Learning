65b5babdd2fb5af26a9dfaad3916b5465728fe69
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-19 12:49:40 +0300

    change config file

--- cars/validate_cars.py
+++ cars/validate_cars.py
@@ -7,14 +7,14 @@ from oml.registry.postprocessors import POSTPROCESSORS_REGISTRY
 from oml.registry.models import PAIRWISE_MODELS_REGISTRY
 from postprocessing.top_freq import TopFrequencyPostprocessor
 from postprocessing.multiple_emb import MultiEmbeddingsPostprocessor
-from models.multi_query_siamese import MultiDistanceSiamese
+from models.multi_query_concat import MultiConcat
+
 
 POSTPROCESSORS_REGISTRY['top_freq'] = TopFrequencyPostprocessor
 POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
-PAIRWISE_MODELS_REGISTRY['multi_query_siamese'] = MultiDistanceSiamese
+PAIRWISE_MODELS_REGISTRY['multi_query_cat'] = MultiConcat
 
-
-@hydra.main(config_path="configs", config_name="val_cars_topFreq.yaml")
+@hydra.main(config_path="configs", config_name="val_cars.yaml")
 def main_hydra(cfg: DictConfig) -> None:
     pl_val(cfg)
 
