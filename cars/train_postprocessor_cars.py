b897ffe032f392ee3487d970306159ac2097e191
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-25 13:50:52 +0300

    upd

--- cars/train_postprocessor_cars.py
+++ cars/train_postprocessor_cars.py
@@ -6,13 +6,15 @@ from utils.train_postprocessor import pl_train_postprocessor
 from oml.registry.postprocessors import POSTPROCESSORS_REGISTRY
 from oml.registry.models import PAIRWISE_MODELS_REGISTRY
 from postprocessing.multiple_emb import MultiEmbeddingsPostprocessor
+from postprocessing.multiple_emb_with_top_freq import MultiEmbeddingsFreqPostprocessor
 from models.multi_query_concat import MultiConcat
 
 
 POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
+POSTPROCESSORS_REGISTRY['multi_emb_freq'] = MultiEmbeddingsFreqPostprocessor
 PAIRWISE_MODELS_REGISTRY['multi_query_cat'] = MultiConcat
 
-@hydra.main(config_path="configs", config_name="train_postprocessor_cars_multiple_emb.yaml")
+@hydra.main(config_path="configs", config_name="train_postprocessor_cars_multi_emb_freq.yaml")
 def main_hydra(cfg: DictConfig) -> None:
     pl_train_postprocessor(cfg)
 
