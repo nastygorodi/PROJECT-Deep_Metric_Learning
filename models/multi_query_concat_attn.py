317e6fb51f6e86753f9303adb2e2ec240e8bddfe
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-29 18:35:56 +0300

    upd

--- models/multi_query_concat_attn.py
+++ models/multi_query_concat_attn.py
@@ -9,32 +9,24 @@ from models.multi_query_concat import remain_prefix_in_state_dict
 
     
 class Attention(torch.nn.Module):
-    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0.0, proj_drop=0.0):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None):
         super().__init__()
         self.num_heads = num_heads
         head_dim = dim // num_heads
         self.scale = qk_scale or head_dim**-0.5
 
-        self.qkv = torch.nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = torch.nn.Dropout(attn_drop)
+        self.q_proj = torch.nn.Linear(dim, dim, bias=qkv_bias)
+        self.k_proj = torch.nn.Linear(dim, dim, bias=qkv_bias)
+        self.v_proj = torch.nn.Linear(dim, dim, bias=qkv_bias)
+        self.mhattn = torch.nn.MultiheadAttention(dim, num_heads, batch_first=True)
 
     def forward(self, x):
-        B, emb_size, n  = x.shape
-        qkv = self.qkv(x.transpose(-1, -2)).reshape(B, -1, n, self.num_heads, emb_size // self.num_heads).permute(1, 0, 2, 3, 4) # 3 x B x n x heads x emb
-        q, k, v = qkv[0], qkv[1], qkv[2] # B x n x heads x dim
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
         
-        q = q.permute(2, 0, 1, 3).reshape(B * self.num_heads, n, -1)
-        k = k.permute(2, 0, 1, 3).reshape(B * self.num_heads, n, -1)
-        v = v.permute(2, 0, 1, 3).reshape(B * self.num_heads, n, -1)
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale # Bheads x n x n
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v)
-        x = x.view(self.num_heads, B, n, -1)
-        x = x.permute(1, 2, 0, 3).reshape(B, n, emb_size).transpose(-1, -2)
-        return x, attn
+        attn, _ = self.mhattn(q, k, v)
+        return attn
 
 
 class MultiConcatWithAttention(IMultiQueryModel):
@@ -60,10 +52,9 @@ class MultiConcatWithAttention(IMultiQueryModel):
 
     def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
         x2_shape = x2.shape
-        res = torch.cat([x1.view(x1.shape[0], -1), x2], dim=-1).view(x2_shape[0], (self.n_query + 1), -1)
-        res = res.transpose(-1, -2) # B x emb_dim x n
-        res_c, _ = self.attn(res)
-        res_out = self.proj(res_c[:, :, -1])
+        res = torch.cat([x1.view(x1.shape[0], -1), x2], dim=-1).view(x2_shape[0], (self.n_query + 1), -1) # B x n x emb_dim
+        res_c = self.attn(res)
+        res_out = self.proj(res_c[:, -1])
         return res_out
 
     def predict(self, x1: Tensor, x2: Tensor) -> Tensor:
