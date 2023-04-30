feb3e14dd37452476ed5c39c31a8ff8001b737d3
nastygorodi <hutao@LAPTOP-QI4DCEM5.localdomain>
2023-04-30 18:00:04 +0300

    upd

--- cars/validate_cars.py
+++ cars/validate_cars.py
@@ -7,12 +7,14 @@ from oml.registry.postprocessors import POSTPROCESSORS_REGISTRY
 from oml.registry.models import PAIRWISE_MODELS_REGISTRY
 from postprocessing.top_freq import TopFrequencyPostprocessor
 from postprocessing.multiple_emb import MultiEmbeddingsPostprocessor
+from postprocessing.multiple_emb_with_top_freq import MultiEmbeddingsFreqPostprocessor
 from models.multi_query_concat import MultiConcat
 from models.multi_query_concat_attn import MultiConcatWithAttention
 
 
 POSTPROCESSORS_REGISTRY['top_freq'] = TopFrequencyPostprocessor
 POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
+POSTPROCESSORS_REGISTRY['multi_emb_freq'] = MultiEmbeddingsFreqPostprocessor
 PAIRWISE_MODELS_REGISTRY['multi_query_cat'] = MultiConcat
 PAIRWISE_MODELS_REGISTRY['multi_query_attn'] = MultiConcatWithAttention
 
