6b756a6bac0c3e4af859a4fdfeafacb01271bb21
nastygorodi <nastygorodi@icloud.com>
2023-05-09 14:41:44 +0300

    add model to registry

--- cars/train_postprocessor_cars.py
+++ cars/train_postprocessor_cars.py
@@ -10,6 +10,7 @@ from postprocessing.multiple_emb_with_top_freq import MultiEmbeddingsFreqPostpro
 from models.multi_query_concat import MultiConcat
 from models.multi_query_concat_attn import MultiConcatWithAttention
 from models.multi_query_mhattn import MultiQueryWithAttention
+from models.multi_query_concat_attn_v2 import MultiConcatWithAttention_v2
 
 
 POSTPROCESSORS_REGISTRY['multiple_emb'] = MultiEmbeddingsPostprocessor
@@ -17,6 +18,7 @@ POSTPROCESSORS_REGISTRY['multi_emb_freq'] = MultiEmbeddingsFreqPostprocessor
 PAIRWISE_MODELS_REGISTRY['multi_query_cat'] = MultiConcat
 PAIRWISE_MODELS_REGISTRY['multi_query_attn'] = MultiConcatWithAttention
 PAIRWISE_MODELS_REGISTRY['multi_query_attn_v2'] = MultiQueryWithAttention
+PAIRWISE_MODELS_REGISTRY['multi_query_attn_v3'] = MultiConcatWithAttention_v2
 
 @hydra.main(config_path="configs", config_name="train_postprocessor_cars_attn.yaml")
 def main_hydra(cfg: DictConfig) -> None:
